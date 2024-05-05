87b2536f0b7b076ae5a70a9c30b5787325a6b3a5
aswadarain786 <162692541+aswadarain786@users.noreply.github.com>
2024-05-06 00:56:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -18,15 +18,15 @@
 //                                                                                            //
 //════════════════════════════════════════════════════════════════════════════════════════════//
 *                                                                 
-  * @project_name : Gifted-Md
-   * @author : Gifted Tech Info
+  * @project_name : M ASWAD 
+   * @author : M ASWAD 
    * @youtube : https://www.youtube.com/@giftedtechnexus
-   * @description : Gifted-Md ,A Multi-functional whatsapp user bot.
+   * @description : M ASWAD ,A Multi-functional whatsapp user bot.
    * @version 2.5.0
 *
 * 
-   * Created By Gifted Tech Info.
-   * © 2024 Gifted-Md.
+   * Created By M ASWAD Tech Info.
+   * © 2024 M ASWAD.
 
 
 */
@@ -70,43 +70,43 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://te
 
 
 
-global.devs = "254728782591,254762016957,254110853827,254728746852,255655147353" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "254728782591,254762016957,254110853827,254728746852,255655147353";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "254728782591,254762016957,254110853827,254728746852,255655147353";
+global.devs = "923419159670" // Developer Contact
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "923419159670";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923419159670";
 
 
 
 
 //========================= [ BOT SETTINGS ] =========================\\
-global.style = process.env.STYLE   || '0',  // put '1' to "5" here to check bot styles
+global.style = process.env.STYLE   || '3',  // put '1' to "5" here to check bot styles
 global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || "false";  // Make it "false" for disable WELCOME 
+global.gdbye = process.env.GOODBYE || "true"; 
+global.wlcm  = process.env.WELCOME || "truee";  // Make it "false" for disable WELCOME 
 
 global.warncount = process.env.WARN_COUNT || 5,
 global.disablepm = process.env.DISABLE_PM || "true",
-global.MsgsInLog = process.env.MSGS_IN_LOG|| "true", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
+global.MsgsInLog = process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
 global.userImages= process.env.USER_IMAGES|| "text",
-global.waPresence= process.env.WAPRESENCE ||  "available" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
+global.waPresence= process.env.WAPRESENCE ||  "recording" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
 global.readcmds = process.env.READ_COMMAND || "true"
-global.readmessage = process.env.READ_MESSAGE || "false"
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "254728782591,254762016957,254110853827,254728746852,255655147353";
+global.readmessage = process.env.READ_MESSAGE || "truee"
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "923xxxxxxxxxx";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "true"
-global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "254xxxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254728782591,254762016957,254110853827,254728746852,255655147353";
+global.save_status = process.env.AUTO_SAVE_STATUS || "true"
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "923xxxxxxxxxx";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923xxxxxxxxxx";
 
 global.api_smd = "https://api-smd.onrender.com" 
 global.scan = "https://session.giftedtechnexus.co.ke";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ; // PUT your SESSION_ID 
+global.SESSION_ID = process.env.SESSION_ID ||  "Gifted;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUc2d045S1FUWUdIaDJCZDVSUzNLUms0RzhmT2VTbFY3UEptZ2hNVmUwTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiM25GTzRDN0tQMjYzTDhTVkJVTXRjZldLQnBQTjVZbm9uK0hMdTlFWlJrZz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwQ1pCUEtaRmZ6V09tVkd4QVkvcUNrSFlzNzZvR0tmbkJIKzlkeFpweEU0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLQnYzZ0hOQVRaTWV3U2hiZCswaVNKOHliRkFjcnlJbDdqWGV1V3dDOFJrPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlNLdzZydUQrbzRlRVJURkd5UHRnbjRqcmZaM2NDWFZzSVVlZzdmMHAwSGM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJYTzNaYVUycVBQYzlabzZXTTFxYXhJY3Z6R0tEZWtNKy9FTDhaMzZ1M289In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieUtvWENYVCtiVExUTUFjYUk1NCtsaEh1eUJXUkF1UVgzR0N3cFZueTVYQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOUhHMmtQU3loZ0RSV3ZEcklnR3BnZHJ4MWRWNCtkclZCSkpiRmhENDJVVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJFNHBZbVQ0eEE4ZktaTDI3T0FYcmhwL2dwYWdmWW5KQlVXREt6eHBjN1pQQUU1MnRwZlMrVldZZVhMOG1jaXBQMTJNNkkxMURmdzc0MmlkclZYb0F3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTM2LCJhZHZTZWNyZXRLZXkiOiJwNWlKdnJYZTBCbHFvQUhKbEwrdE5VdlVnb1UrNTM2NzFYUC9uallHZUhNPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjkyMzQxOTE1OTY3MEBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiIwREIzNERCRjE2RTFFRTg2MzY2MEYzMjQwODlCOUI1NSJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzE0OTM4NTE1fV0sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjoxLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiI3UzFTTVlubFRIR010aGVGcWxKdC1nIiwicGhvbmVJZCI6IjdjMmM1ZmNkLTE3ZWItNDU0MS1iMmY1LWZjNTc5MjViNzhiMiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNdFptZm5LNmR5UjFLTkJGQkJuQ2Z0MlJuNlU9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSWdBRG4vODQrWG13MmRIeCt0UnFVeWovQ1dRPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjgzVjhLOU1YIiwibWUiOnsiaWQiOiI5MjM0MTkxNTk2NzA6NTFAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoiKn4ifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0orYXJNY0ZFSVBGMzdFR0dBSWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InNiN3ovRFY4Q3I0c09HSXZJZ1dVRElWR1dVaUhUZDdwaVpDWEh3dGRieWs9IiwiYWNjb3VudFNpZ25hdHVyZSI6Ikdac0cwQzVlaGR5YUttUmp4WHpVMjl6Q3VYazJjZ09LNUJQVVdYalFWcGVuMFpiQXFCWHZiaXNkbUFYSmdBVERVTTRQMllzN3IrKzA0ZkNiTVNWTkFnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJCNTlOT1BaS3lxR2prWTlaT25XSU5mMnZpODN5dGNENms1VTBLWWZCb2c0UDhFQ1QyYkl5SmRnK3V2endicUNXRzUzaldHeVo2bHlDUTFENXc0NWdDQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzQxOTE1OTY3MDo1MUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJiRys4L3cxZkFxK0xEaGlMeUlGbEF5RlJsbEloMDNlNlltUWx4OExYVzhwIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzE0OTM4NTEyLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUNORiJ9" ; // PUT your SESSION_ID 
 
 
 module.exports = {
@@ -116,12 +116,12 @@ module.exports = {
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "V.2.5.1",
-  caption : process.env.CAPTION || "*©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs*" , // ```『 ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs 』```", //*『ᴠᴇɴᴏᴄʏʙᴇʀ ᴍᴅ』*\n youtube.com/@giftedtechnexus"),
+  caption : process.env.CAPTION || "*©²⁰²⁴ M ASWAD ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs*" , // ```『 ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs 』```", //*『ᴠᴇɴᴏᴄʏʙᴇʀ ᴍᴅ』*\n youtube.com/@giftedtechnexus"),
  
-  author : process.env.PACK_AUTHER|| "Gifted Tech",
-  packname: process.env.PACK_NAME || "Gifted-Md♥️",
-  botname : process.env.BOT_NAME  || "ɢɪғᴛᴇᴅ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "Gifted Tech",
+  author : process.env.PACK_AUTHER|| "M ASWAD",
+  packname: process.env.PACK_NAME || "M ASWAD",
+  botname : process.env.BOT_NAME  || "M ASWAD",
+  ownername:process.env.OWNER_NAME|| "M ASWAD",
 
 
   errorChat : process.env.ERROR_CHAT || "",
@@ -135,9 +135,9 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
-  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "private",
-  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "Gifted",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "a0e25c3901a992c4afc2ccd14c87f9ec",
+  WORKTYPE: process.env.WORKTYPE||process.env.MODE || "public",
+  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "M ASWAD",
 
 
 
